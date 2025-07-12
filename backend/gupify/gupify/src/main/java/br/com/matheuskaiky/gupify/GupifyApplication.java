bfa1e30afcb99fd63c9b2514eb5a00074d2e28c3
Matheus <matheusmarimath@gmail.com>
2025-07-12 20:26:37 -0300

    defined Job domain class #2

---
+++ backend/gupify/gupify/src/main/java/br/com/matheuskaiky/gupify/GupifyApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheuskaiky.gupify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GupifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GupifyApplication.class, args);
+	}
+
+}
