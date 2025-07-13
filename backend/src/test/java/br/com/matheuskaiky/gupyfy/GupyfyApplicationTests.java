5516d41ecf0cc41496010c2793a68d5091f5d399
Matheus <matheusmarimath@gmail.com>
2025-07-12 21:35:30 -0300

    added mock job data #3

--- backend/src/test/java/br/com/matheuskaiky/gupyfy/GupyfyApplicationTests.java
+++ backend/src/test/java/br/com/matheuskaiky/gupyfy/GupyfyApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.matheuskaiky.gupify;
+package br.com.matheuskaiky.gupyfy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GupifyApplicationTests {
+class GupyfyApplicationTests {
 
 	@Test
 	void contextLoads() {
