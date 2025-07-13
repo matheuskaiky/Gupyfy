6adce2b8bb201af8c5395d028a9504d18ff0ba96
Matheus <matheusmarimath@gmail.com>
2025-07-12 22:10:31 -0300

    close #6 and #7

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [jobs, setJobs] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/jobs')
+            .then(response => {
+                console.log('Dados recebidos da API:', response.data);
+                setJobs(response.data);
+            })
+            .catch(error => {
+                console.error('Houve um erro ao buscar as vagas:', error);
+            });
+    }, []);
+
+    return (
+        <>
+            <h1>Gupyfy Job Hunter 🚀</h1>
+            <h2>Vagas Encontradas:</h2>
+            <ul>
+                {}
+                {jobs.map(job => (
+                    <li key={job.id}>
+                        {job.title} - {job.companyName}
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
 
-export default App
+export default App;
