6e24b46f16b80dbfe7ff6f6498f5aeec7e4fb3c7
Matheus <matheusmarimath@gmail.com>
2025-08-20 14:41:53 -0300

    refactor(persistence): migrate database from sqlite to postgresql #41

--- backend/src/main/java/br/com/matheuskaiky/gupyfy/GupyfyApplication.java
+++ backend/src/main/java/br/com/matheuskaiky/gupyfy/GupyfyApplication.java
@@ -3,6 +3,7 @@ package br.com.matheuskaiky.gupyfy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.hibernate.*;
 
 @EnableScheduling
 @SpringBootApplication
